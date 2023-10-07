eccabd43fbd4625caa333cfe96dc349f870e1a59
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-07 17:12:57 +0300

    deleted react icons and changed title index.html

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <h1>Teknolojik Yemekler</h1>
-      <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
+      
     </>
   );
 };
